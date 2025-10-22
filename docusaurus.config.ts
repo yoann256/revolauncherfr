79aacec0c50310ef8d3c4e9c0a5b8c783ede5687
yoann256 <yoanndegardin03@gmail.com>
2025-10-22 11:24:35 +0200

    translation test fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/yoann256/revolauncherfr',
           label: 'GitHub',
           position: 'right',
         },
